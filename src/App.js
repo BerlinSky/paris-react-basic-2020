86e58621e973be5db86b840a1eb5f8244cfb8c75
Berlin Sky <pedroyoung8989@gmail.com>
2020-02-08 11:28:36 -0800

    Understand the createElement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react'
 // import { hot } from 'react-hot-loader'
 
+// # 7
+// const elemClass = 'siteLayout2'
+// const elem = 'Baseline 5'
+// const App = <div className={ elemClass + ' layout--modifier'}>{ (() => elem) () }</div>
+
+// # 6
+// const elemClass = 'siteLayout2'
+// const elem = 'Baseline 4'
+// const App = <div className={ elemClass }>{ (() => elem) () }</div>
+
+// # 5
+// const elem = 'Baseline 3'
+// const App = <div className="siteLayout">{ (() => elem) () }</div>
+
+// # 4
+// const elem = 'Baseline 2'
+// const App = <div className="siteLayout">{ elem }</div>
+
+// # 3
+// const App = <div className="siteLayout">Baseline Site</div>
+
+// # 2
 // const App = React.createElement('div', {
 //   className: 'siteLayout',
 //   children: ['Baseline Site', 'Just a starting point']
 // })
 
+// #1
 const App = React.createElement(
   'div',
   { className: 'siteLayout' },
@@ -18,7 +41,6 @@ const App = React.createElement(
 //     return (
 //       <div>
 //         <h1>Hello World.</h1>
-//         { elm }
 //       </div>
 //     )
 //   }
