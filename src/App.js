9d7c84f8a21f11b9145469561b6e6596d0adab7b
Berlin Sky <pedroyoung8989@gmail.com>
2020-02-08 16:19:06 -0800

    More react basic with props

--- src/App.js
+++ src/App.js
@@ -1,40 +1,23 @@
 import React from 'react'
 // import { hot } from 'react-hot-loader'
 
-// # 7
-// const elemClass = 'siteLayout2'
-// const elem = 'Baseline 5'
-// const App = <div className={ elemClass + ' layout--modifier'}>{ (() => elem) () }</div>
-
-// # 6
-// const elemClass = 'siteLayout2'
-// const elem = 'Baseline 4'
-// const App = <div className={ elemClass }>{ (() => elem) () }</div>
-
-// # 5
-// const elem = 'Baseline 3'
-// const App = <div className="siteLayout">{ (() => elem) () }</div>
-
-// # 4
-// const elem = 'Baseline 2'
-// const App = <div className="siteLayout">{ elem }</div>
-
-// # 3
-// const App = <div className="siteLayout">Baseline Site</div>
-
-// # 2
-// const App = React.createElement('div', {
-//   className: 'siteLayout',
-//   children: ['Baseline Site', 'Just a starting point']
-// })
-
-// #1
-const App = React.createElement(
-  'div',
-  { className: 'siteLayout' },
-  'Baseline Site',
-  ' Just a starting point'
+// # 9: Override the props.className:
+const props = {
+  className: 'siteLayout',
+  children: 'Baselisne Site 8'
+}
+const App = (
+  <div {...props} className="fancyClass">
+    New Baseline Site
+  </div>
 )
+
+// // # 8
+// const props = {
+//   className: 'siteLayout',
+//   children: 'Baselise Site 8'
+// }
+// const App = <div { ...props } />
 
 // class App extends React.Component {
 //   render() {
